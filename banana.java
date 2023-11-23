a5819ee6317bda4dfd3344b582a2a5a6c660d9e8
gabriel-chu <350051942@gapps.yrdsb.ca>
2023-11-23 09:08:26 -0500

    banana added

--- banana.java
+++ banana.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class banana extends Actor
+public class Banana extends Actor
 {
     /**
      * Act - do whatever the banana wants to do. This method is called whenever
