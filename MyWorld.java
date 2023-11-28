22f512b97aac588f1eb2bc7322b1dba586f5adb6
gabriel-chu <350051942@gapps.yrdsb.ca>
2023-11-28 08:16:34 -0500

    elephant can move left and right

--- MyWorld.java
+++ MyWorld.java
@@ -18,7 +18,24 @@ public class MyWorld extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
         
+        Elephant elephant = new Elephant();
+        addObject(elephant, 300, 200);
         
-       
+        createBanana();
+    }
+
+    /**
+     * create a new banana at random location
+     */
+    public void createBanana()
+    {
+        Banana banana = new Banana();
+        int x = Greenfoot.getRandomNumber(600);
+        int y = 0;
+        addObject(banana, x, y );
     }
 }
+
+
+    
+
--- elephant.java
+++ elephant.java
@@ -15,6 +15,30 @@ public class Elephant extends Actor
     public void act()
     {
         // Add your action code here.
-        move(1);
+        if(Greenfoot.isKeyDown("left"))
+        {
+            move(-2);
+        }
+        else if(Greenfoot.isKeyDown("right"))
+        {
+            move(2);
+        }
+        
+        // Remove banana if elephant eats it
+        eat();
+    }
+    
+    /**
+     * Eat the banana and spawn new banana if banana is eaten
+     */
+    public void eat()
+    {
+        if(isTouching(Banana.class))
+        {
+            removeTouching(Banana.class);
+            MyWorld world = (MyWorld) getWorld();
+            world.createBanana();
+        }
+        removeTouching(Banana.class);
     }
 }
